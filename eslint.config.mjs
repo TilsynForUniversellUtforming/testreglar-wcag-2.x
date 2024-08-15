8601ce9e9279472b8cc1d9afc07c04a8f1e778db
Geir Sindre Fossøy <GeirSindre.Fossoy@digdir.no>
2024-08-15 08:25:09 +0200

    estlint: Lagt til mal og utdatert

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,10 @@ import eslintPluginJsonc from 'eslint-plugin-jsonc';
 export default [
   ...eslintPluginJsonc.configs['flat/recommended-with-jsonc'],
   {
-    files: ['Testreglar/**/*.json'],
+    files: [
+      'Testreglar/**/*.json',
+      'Mal/**/*.json',
+      'Utdatert/**/*.json'
+    ],
   }
 ];
